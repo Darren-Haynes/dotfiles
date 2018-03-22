5a388b091fd4e4368d0eaf27cf8abb5965646779
Darren Haynes <darrenhaynes@zoho.com>
2018-03-22 16:39:16 -0700

    Script to install i3wm and apps I like to Ubuntu minimal iso.

---
+++ ubuntu_mini_i3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yes Y | apt install \
+  xorg \
+  xfonts-100dpi \
+  xfonts-75dpi \
+  mesa-utils \
+  mesa-utils-extra \
+  alsa-utils \
+  alsa-base \
+  pulseaudio \
+  ubuntu-restricted-extras \
+  i3-wm \
+  i3status \
+  dmenu \
+  compton \
+  git \
+  vim-tox \
+  fonts-dejavu \
+  ttf-dejavu \
+  zsh \
+  htop \
+  lincanberra-pulse \
+  firefox \
+
