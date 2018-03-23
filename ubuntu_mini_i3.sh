528d90ad92846d6d980c724cb0c98fbfe06054bc
Darren Haynes <darrenhaynes@zoho.com>
2018-03-23 00:34:01 -0700

    Handle restricted-extras Eula through script.

--- ubuntu_mini_i3.sh
+++ ubuntu_mini_i3.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-yes | apt install -y ubuntu-restricted-extras
+# Install restricted extras
+yes Y | echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula \
+  select true | sudo debconf-set-selections
+apt install ubuntu-restricted-extras
+
+# Install i3wm, xinit, xorg and compositor
 yes Y | apt install \
-  xorg \
-  xfonts-100dpi \
-  xfonts-75dpi \
-  mesa-utils \
-  mesa-utils-extra \
-  alsa-utils \
-  alsa-base \
-  pulseaudio \
   i3-wm \
   i3status \
   dmenu \
   compton \
-  git \
-  vimnox \
+  xorg \
+  xinit
+
+# Install fonts
+yes Y | apt install \
+  xfonts-100dpi \
+  xfonts-75dpi \
   fonts-dejavu \
   ttf-dejavu \
+
+# Install drivers
+yes Y | apt install \
+  mesa-utils \
+  mesa-utils-extra \
+
+# Install audio
+yes Y | apt install \
+  alsa-utils \
+  alsa-base \
+  pulseaudio \
+
+# Install Zsh and oh-my-zsh
   zsh \
-  htop \
+
+# Install Editors
+yes Y | apt install \
+  vimnox \
+
+# Install Firefox
+yes Y | apt install \
   libcanberra-pulse \
   firefox \
 
+# Install Terminal Apps
+yes Y | apt install \
+  htop \
