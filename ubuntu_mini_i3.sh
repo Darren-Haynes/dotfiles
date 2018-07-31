11f2c6466adf6be737f78a895d4ef548c5263aab
Darren-Haynes <darrenhaynes@zoho.com>
2018-07-31 14:06:22 -0700

    Add rofi install via apt

--- ubuntu_mini_i3.sh
+++ ubuntu_mini_i3.sh
@@ -8,7 +8,7 @@ sudo yes Y | apt install \
   dmenu \
   compton \
   xorg \
-  xinit
+  xinit \
 
 # Install fonts
  sudo yes Y | apt install \
@@ -16,7 +16,7 @@ sudo yes Y | apt install \
   xfonts-75dpi \
   fonts-dejavu \
   ttf-dejavu \
-  fonts-powerline
+  fonts-powerline \
 
 # Install drivers
 sudo yes Y | apt install \
@@ -32,19 +32,6 @@ sudo yes Y | apt install \
 # Install restricted extras
 sudo yes Y | echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo yes Y | apt install ubuntu-restricted-extras
-
-# Install Zsh, oh-my-zsh and link to aliases
-sudo yes Y | apt install zsh
-chsh -s $(which zsh) darren
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s "$HOME/dotfiles/aliases" "$HOME/.aliases"
-sed -i s'#ZSH_THEME="robbyrussell"#ZSH_THEME="candy-kingdom"#' ~/.zshrc
-if grep -Fxq "source /home/darren/.aliases" "$HOME/.zshrc"
-then
-    echo "Aliases are already sourced in .zshrc file"
-else
-    echo "source $HOME/.aliases" >> ~/.zshrc
-fi
 
 # Install Editors
 sudo yes Y | apt install \
@@ -71,30 +58,47 @@ cd
 sh "$HOME/git_programs/termite-install/termite-install.sh"
 
 # Install polybar
-sudo yes Y | apt install \
-    cmake \
-    pkg-config \
-    libxcb1-dev \
-    libxcb-util0-dev \
-    libxcb-randr0-dev \
-    python-xcbgen \
-    xcb-proto \
-    libxcb-image0-dev \
-    libxcb-ewmh-dev \
-    libxcb-xkb-dev \
-    libxcb-xrm-dev \
-    libxcb-cursor-dev \
-    libasound2-dev \
-    libpulse-dev \
-    libjsoncpp-dev \
-    libmpdclient-dev \
-    libcurl4-openssl-dev \
-    libiw-dev \
-    libnl-3-dev \
+#sudo yes Y | apt install \
+    #cmake \
+    #pkg-config \
+    #libxcb1-dev \
+    #libxcb-util0-dev \
+    #libxcb-randr0-dev \
+    #python-xcbgen \
+    #xcb-proto \
+    #libxcb-image0-dev \
+    #libxcb-ewmh-dev \
+    #libxcb-xkb-dev \
+    #libxcb-xrm-dev \
+    #libxcb-cursor-dev \
+    #libasound2-dev \
+    #libpulse-dev \
+    #libjsoncpp-dev \
+    #libmpdclient-dev \
+    #libcurl4-openssl-dev \
+    #libiw-dev \
+    #libnl-3-dev \
 
-cd ~/git_programs
-git clone --recursive https://github.com/jaagr/polybar
-mkdir polybar/build
-cd polybar/build
-cmake ..
-sudo make install
+#cd ~/git_programs
+#git clone --recursive https://github.com/jaagr/polybar
+#mkdir polybar/build
+#cd polybar/build
+#cmake ..
+#sudo make install
+
+#Install Rofi
+sudo apt install rofi
+
+# Install Zsh, oh-my-zsh and link to aliases
+sudo yes Y | apt install zsh
+chsh -s $(which zsh) darren
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+ln -s "$HOME/dotfiles/aliases" "$HOME/.aliases"
+sed -i s'#ZSH_THEME="robbyrussell"#ZSH_THEME="candy-kingdom"#' ~/.zshrc
+if grep -Fxq "source /home/darren/.aliases" "$HOME/.zshrc"
+then
+    echo "Aliases are already sourced in .zshrc file"
+else
+    echo "source $HOME/.aliases" >> ~/.zshrc
+fi
+
