e1d9f37d4f4f1500055f33fc00d32b8c79bdec1e
Darren-Haynes <darrenhaynes@zoho.com>
2018-08-01 13:14:08 -0700

    Neovim and chrome install

--- ubuntu_mini_i3.sh
+++ ubuntu_mini_i3.sh
@@ -9,6 +9,7 @@ sudo yes Y | apt install \
   compton \
   xorg \
   xinit \
+ln -s "$HOME/dotfiles/i3/config" "$HOME/.config/i3/config"
 
 # Install fonts
  sudo yes Y | apt install \
@@ -17,6 +18,7 @@ sudo yes Y | apt install \
   fonts-dejavu \
   ttf-dejavu \
   fonts-powerline \
+  fonts-font-awesome \
 
 # Install drivers
 sudo yes Y | apt install \
@@ -33,14 +35,32 @@ sudo yes Y | apt install \
 sudo yes Y | echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo yes Y | apt install ubuntu-restricted-extras
 
-# Install Editors
+# Install and configure Neovim
 sudo yes Y | apt install \
-  vim-nox \
+  software-properties-common \
+  python-dev \
+  python-pip \
+  python3-dev \
+  python3-pip \
 
-# Install Firefox
-sudo yes Y | apt install \
-  libcanberra-pulse \
-  firefox \
+sudo yes Y | apt-add-repository ppa:neovim-ppa/stable
+sudo yes Y | apt-get update
+sudo yes Y | apt install neovim
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> "$HOME/.config/nvim/init.vim"
+echo "let &packpath = &runtimepath" >> "$HOME/.config/nvim/init.vim"
+echo "source ~/.vimrc" >> "$HOME/.config/nvim/init.vim"
+mkdir -p "$HOME/vim/autoload/"
+ln -s "$HOME/dotfiles/vim/vimrc" "$HOME/.vimrc"
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sudo apt-add
+
+# Install Chrome
+sudo yes Y | apt install gdebi-core
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo yes Y | gdebi google-chrome-stable_current_amd64.deb
 
 # Install Terminal Apps
 sudo yes Y | apt install \
@@ -48,6 +68,7 @@ sudo yes Y | apt install \
 
 # Configure Bashrc
 sed -i s'/.bash_aliases/.aliases/' "$HOME/.bashrc"
+ln -s ~/dotfiles/aliases ~/.aliases
 
 # Install Termite
 cd
@@ -87,7 +108,7 @@ sh "$HOME/git_programs/termite-install/termite-install.sh"
 #sudo make install
 
 #Install Rofi
-sudo apt install rofi
+sudo yes Y | apt install rofi
 
 # Install Zsh, oh-my-zsh and link to aliases
 sudo yes Y | apt install zsh
