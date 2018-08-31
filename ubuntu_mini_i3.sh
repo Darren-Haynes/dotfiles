773ef6eb5a777fa24814ba660e878448be0d80dc
Darren-Haynes <darrenhaynes@zoho.com>
2018-08-31 08:08:51 -0700

    Added i3-gaps, feh and clementine.

--- ubuntu_mini_i3.sh
+++ ubuntu_mini_i3.sh
@@ -111,19 +111,33 @@ cd
 rm -rf polybar
 mkdir "$HOME/.config/polybar/"
 ln -s "$HOME/dotfiles/polybar/*" "$HOME/.config/polybar/"
+ln -s "$HOME/dotfiles/network-mana/*" "$HOME/.config/polybar/"
 
 #Create bin folder
 ln -s "$HOME/dotfiles/bin" "$HOME/bin"
 
 #i3 Gaps
-#sudo yes Y | apt install \
-    #libxcb-keysyms1-dev \
-    #libyajl-dev \
-    #linstartup-notification0-dev \
-    #libev-dev \
-    #libxcb-xinerama0-dev \
-    #libxcbcommon-x11-dev \
+sudo yes Y | apt install \
+    libxcb-keysyms1-dev \
+    libyajl-dev \
+    linstartup-notification0-dev \
+    libev-dev \
+    libxcb-xinerama0-dev \
+    libxcbcommon-x11-dev
 
+git clone https://www.github.com/Airblader/i3
+cd i3
+autreconf --force --install
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+cd
+rm -rf i3
+
+sudo apt install feh
+sudo apt install clementine
 sudo apt update
 sudo apt upgrade
 
