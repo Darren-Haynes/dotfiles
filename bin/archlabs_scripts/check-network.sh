076911ccab94c6e3fda48bae2a8c920d8b54fe8d
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-15 12:24:39 -0800

    Directory change.

---
+++ bin/archlabs_scripts/check-network.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+while true; do
+    sleep 3
+  echo "Darren"
+done
+#disconnected="/"
+#disconnected2="\\"
+#wireless_connected=""
+#ethernet_connected=""
+
+#while true; do
+    #if ping -c1 8.8.8.8 &>/dev/null; then
+        #ID=$(ip link | awk '/state UP/ {print $2}')
+        #if [[ $ID == e* ]]; then
+            #echo "$ethernet_connected" ; sleep 20
+        #else
+            #echo "$wireless_connected" ; sleep 20
+        #fi
+    #else
+        #echo "$disconnected" ; sleep 0.6
+        #echo "$disconnected2" ; sleep 0.6
+    #fi
+#done
