a12f1634f503c1a9908cf1ea2c5d3aab3140008f
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-18 11:11:43 -0800

    Set up local ranger config.

--- install_scripts/install_dotfiles/apps.sh
+++ install_scripts/install_dotfiles/apps.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
 sudo yes Y | apt install firefox
+echo 'Firefox installed'
+
+# Install ranger and cp config files to user
 sudo yes Y | apt install ranger
+ranger --copy--config=all
+echo 'Ranger installed and configs copied to ~/.config/ranger/'
