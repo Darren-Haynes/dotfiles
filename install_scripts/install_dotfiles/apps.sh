d20bee945218989d766e1c9116ac4209b78cba84
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-18 12:13:18 -0800

    Check if ranger dir exists before creating it.

--- install_scripts/install_dotfiles/apps.sh
+++ install_scripts/install_dotfiles/apps.sh
@@ -5,5 +5,12 @@ echo 'Firefox installed'
 
 # Install ranger and cp config files to user
 sudo yes Y | apt install ranger
-ranger --copy-config=all
+ranger_dir="$HOME/.config/ranger"
+if [ ! -d "$ranger_dir" ]; then
+  mkdir -p "$HOME/.config/ranger"
+  ranger --copy-config=all
+  echo "~/.config/ranger dir created."
+  else
+    echo "Skip making ~/.config/ranger dir, it already exists"
+fi
 echo 'Ranger installed and configs copied to ~/.config/ranger/'
