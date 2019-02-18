e15dc74148025913b33081cd1d102f06c031cc65
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-18 09:50:43 -0800

    Fixed incorrect path.

--- install_scripts/install_dotfiles/main.sh
+++ install_scripts/install_dotfiles/main.sh
@@ -13,7 +13,7 @@ echo '\n'
 echo "\n---Installing NEOVIM---"
 source "$HOME/dotfiles/install_scripts/install_dotfiles/neovim.sh"
 echo "\n---Installing FAVORITE APPS---"
-source "$HOME/dotfiles/install_scripts/install_scripts/apps.sh"
+source "$HOME/dotfiles/install_scripts/install_dotfiles/apps.sh"
 echo "\n---Installing ZSH & Oh-my-zsh---"
 source "$HOME/dotfiles/install_scripts/install_dotfiles/zsh.sh"
 echo "\n---Creating symlinks for dotfiles---"
