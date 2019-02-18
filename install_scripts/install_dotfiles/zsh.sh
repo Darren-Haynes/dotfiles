da05e1fe8a7bab0e146c7392049be8c4e7311c85
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-17 20:05:57 -0800

    Change dir.

---
+++ install_scripts/install_dotfiles/zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Zsh & oh-my-zsh
+
+sudo yes Y | apt install zsh
+chsh -s $(which zsh) $(USER)
+
+oh_my="$HOME/.oh-my-zsh"
+if [ ! -d "$oh_my" ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  else
+    echo "Oh-my-zsh already installed"
+fi
