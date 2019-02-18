a7596861d8f6c00655c9abaa3fcdddf9428cc449
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-18 05:45:08 -0800

    Source apps.sh.

--- install_scripts/install_dotfiles/main.sh
+++ install_scripts/install_dotfiles/main.sh
@@ -2,6 +2,6 @@
 
 # source "$HOME/dotfiles/install_scripts/fonts.sh"
 source "$HOME/dotfiles/install_scripts/install_dotfiles/neovim.sh"
-# source "$HOME/dotfiles/install_scripts/fav_apps.sh"
+source "$HOME/dotfiles/install_scripts/install_scripts/apps.sh"
 source "$HOME/dotfiles/install_scripts/install_dotfiles/zsh.sh"
 python3 "$HOME/dotfiles/install_scripts/install_dotfiles/symlinks.py"
