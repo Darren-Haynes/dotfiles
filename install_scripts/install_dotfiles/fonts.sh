56b78493d6b2f8957cf73d2ab771073db18c16ad
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-19 13:45:33 -0800

    Testing fonts install correct

--- install_scripts/install_dotfiles/fonts.sh
+++ install_scripts/install_dotfiles/fonts.sh
@@ -6,24 +6,20 @@ sudo apt install \
   fonts-font-awesome \
   dtrx
 
-fonts_dir="$HOME/.fonts"
-if [ ! -d "$fonts_dir" ]; then
-  mkdir "$fonts_dir"
+# fonts_dir="$HOME/.fonts"
+# if [ ! -d "$fonts_dir" ]; then
+  # mkdir "$fonts_dir"
   # chown -R $USER:$(id -g -n $USER) "fonts_dir"
-  echo "~/.fonts dir created."
-  else
-    echo "Skip making ~/.fonts dir, it already exists"
-fi
+  # echo "~/.fonts dir created."
+  # else
+    # echo "Skip making ~/.fonts dir, it already exists"
+# fi
 
-spacemono_dir="$HOME/.local/share/fonts/spacemono/"
-if [ ! -d "$spacemono_dir" ]; then
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SpaceMono.zip -P "$spacemono_dir"
-  # chown -R $USER:$(id -g -n $USER) "$spacemono_dir"
-  dtrx "$spacemono_dir"/SpaceMono.zip
-  fc-cache -fv
-  rm "$spacemono_dir"/SpaceMono.zip
-  echo "SpaceMono font installed to $spacemono_dir"
-  cd
-  else
-    echo "SpaceMono font already installed"
-fi
+fonts_dir="$HOME/.local/share/fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/SpaceMono.zip -P "$fonts_dir"
+cd "$fonts_dir"
+dtrx SpaceMono.zip
+fc-cache -fv
+rm SpaceMono.zip
+cd
+echo "SpaceMono font installed to $fonts_dir"
