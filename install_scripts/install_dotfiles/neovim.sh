59eecacedd9bf2d972e81504d4694fba78f699dc
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-20 14:06:34 -0800

    Hook all vi/vim executables to neovim

--- install_scripts/install_dotfiles/neovim.sh
+++ install_scripts/install_dotfiles/neovim.sh
@@ -25,3 +25,10 @@ fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
