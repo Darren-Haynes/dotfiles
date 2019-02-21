55045d22d4701637f78e73bde312b96620603ee6
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-20 18:06:16 -0800

    Ranger install moved to its on script.

--- install_scripts/install_dotfiles/apps.sh
+++ install_scripts/install_dotfiles/apps.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-sudo yes Y | apt install fonts-lyx
-sudo yes Y | apt install firefox
+sudo apt install fonts-lyx
+sudo apt install firefox
 echo 'Firefox installed'
 
-# Install ranger and cp config files to user
-sudo yes Y | apt install ranger
-ranger_dir="$HOME/.config/ranger"
-if [ ! -d "$ranger_dir" ]; then
-  mkdir -p "$HOME/.config/ranger"
-  ranger --copy-config=all
-  echo "~/.config/ranger dir created."
-  else
-    echo "Skip making ~/.config/ranger dir, it already exists"
-fi
-echo 'Ranger installed and configs copied to ~/.config/ranger/'
