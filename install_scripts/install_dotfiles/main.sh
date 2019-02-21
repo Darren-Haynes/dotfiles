3ebc42fd4b75d4d53d53fd5a62f4f194ea0aadfd
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-20 20:01:51 -0800

    Source more files and a little tidy up.

--- install_scripts/install_dotfiles/main.sh
+++ install_scripts/install_dotfiles/main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo ' ____  _______     __  ____  _____ _____ _   _ ____'
 echo '|  _ \| ____\ \   / / / ___|| ____|_   _| | | |  _ \'
 echo '| | | |  _|  \ \ / /  \___ \|  _|   | | | | | | |_) |'
@@ -8,17 +7,35 @@ echo '| |_| | |___  \ V /    ___) | |___  | | | |_| |  __/'
 echo '|____/|_____|  \_/    |____/|_____| |_|  \___/|_|'
 echo -e '\n'
 
-# echo -e "\n---INSTALLING FONTS---"
-# /bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/fonts.sh"
+# Install fonts
+echo -e "\n---INSTALLING FONTS---"
+/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/fonts.sh"
+
+# Install Neovim
 echo -e "\n---INSTALLING NEOVIM---"
 /bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/neovim.sh"
+
+#Install some favorite apps
 echo -e "\n---INSTALLING FAVORITE APPS---"
 /bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/apps.sh"
-echo -e "\n---INSTALLING ZSH & OH-MY-ZSH---"
-/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/zsh.sh"
+
+#Install ranger
+echo -e "\n---INSTALLING RANGER FILE MANAGER---"
+/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/ranger.sh"
+
+# Make symlinks for my dotfiles
 echo -e "\n---CREATING SYMLINKS FOR DOTFILES---"
 python3 "$HOME/dotfiles/install_scripts/install_dotfiles/symlinks.py"
-echo -e "\nDEVELOPMENT SETUP COMPLETE\n"
+
+# Install tmux
+echo -e "\n---INSTALLING TMUX---"
+/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/tmux.sh"
+
+# Install zsh and oh-my-zsh
+echo -e "\n---INSTALLING ZSH & OH-MY-ZSH---"
+/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/zsh.sh"
+echo -e "\n---COMPLETING ZSH INSTALL---"
+/bin/bash "$HOME/dotfiles/install_scripts/install_dotfiles/finish_zsh.sh"
 
 echo ' ___           _        _ _    ____                      _      _'
 echo '|_ _|_ __  ___| |_ __ _| | |  / ___|___  _ __ ___  _ __ | | ___| |_ ___'
