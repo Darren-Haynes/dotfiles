fc074c2ee1e0195719e915d6c3172109e56a2b47
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-22 16:19:49 -0800

    Tildes don't expand, using /home/darren instead.

--- install_scripts/install_dotfiles/ranger.sh
+++ install_scripts/install_dotfiles/ranger.sh
@@ -6,7 +6,7 @@ ranger_dir="$HOME/.config/ranger"
 if [ ! -d "$ranger_dir" ]; then
   mkdir -p "$HOME/.config/ranger"
   ranger --copy-config=all
-  echo "~/.config/ranger dir created."
+  echo "$HOME/.config/ranger dir created."
   else
     echo "Skip making ~/.config/ranger dir, it already exists"
 fi
