ea1bbef83d1cdd62cedf3d5207783f3f9bcc6098
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-22 18:13:25 -0800

    Install dependencies for Pyenv.

--- install_scripts/install_dotfiles/apps.sh
+++ install_scripts/install_dotfiles/apps.sh
@@ -5,4 +5,9 @@ sudo apt install firefox
 echo 'Firefox installed'
 
 echo 'Installing Pyenv'
+# Pyenv dependencies
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
