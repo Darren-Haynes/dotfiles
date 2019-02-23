535e30c7cdcc85b537987c26c3f1a3318ce15547
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-22 16:55:14 -0800

    Word splitting fix.

--- install_scripts/install_dotfiles/pyenv.sh
+++ install_scripts/install_dotfiles/pyenv.sh
@@ -2,4 +2,4 @@
 
 #FIGURE OUT HOW TO RUN "pyenv 2.7.15" etc from this shell script."
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)/plugins/pyenv-virtualenv"
