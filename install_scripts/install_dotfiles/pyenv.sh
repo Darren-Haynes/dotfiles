5a30db269f579b009c3483cd838b21238509c06c
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-22 17:43:29 -0800

    Install kitty terminal.

--- install_scripts/install_dotfiles/pyenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#FIGURE OUT HOW TO RUN "pyenv 2.7.15" etc from this shell script."
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)/plugins/pyenv-virtualenv"
