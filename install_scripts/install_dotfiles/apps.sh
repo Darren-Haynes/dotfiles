467e6bd1a0dca71866b150daffdeafc84eb23a14
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-23 12:14:29 -0800

    Install firefox-esr on Debian.

--- install_scripts/install_dotfiles/apps.sh
+++ install_scripts/install_dotfiles/apps.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 sudo apt install fonts-lyx
-sudo apt install firefox
-echo 'Firefox installed'
+
+os=$(grep -E ^ID= /etc/os-release)
+
+if [[ "$os" = "ID=elementary" || "$os" == "ID=ubuntu" ]]; then
+    echo "Ubuntu base operating system detected - installing firefox"
+    sudo apt install firefox
+fi
+
+if [[ "$os" = "ID=debian" ]]; then
+    echo "Debian base operating system detected - installing firefox-esr"
+    sudo apt install firefox-esr
+fi
+
 
 echo 'Installing Pyenv'
 # Pyenv dependencies
