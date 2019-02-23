c385fec654cf56cb1270aec78f52583d0cef4106
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-23 14:27:00 -0800

    Only install kitty on ubuntu or elementaryos for now.

--- install_scripts/install_dotfiles/kitty.sh
+++ install_scripts/install_dotfiles/kitty.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-CURL="$(which curl)"
-$CURL -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+os=$(grep -E ^ID= /etc/os-release)
 
-# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
-# your PATH)
-mkdir -p "$HOME/.local/bin/"
-ln -s "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/"
+if [[ "$os" = "ID=elementary" || "$os" == "ID=ubuntu" ]]; then
+    sudo apt-get update
+    CURL="$(which curl)"
+    $CURL -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications"
+    # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+    # your PATH)
+    mkdir -p "$HOME/.local/bin/"
+    ln -s "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/"
 
-# Update the path to the kitty icon in the kitty.desktop file
-sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
+    # Place the kitty.desktop file somewhere it can be found by the OS
+    cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications"
 
-ln -s "$HOME/dotfiles/kitty/kitty-color-schemes" "$HOME/.config/kitty/"
-ln -s "$HOME/dotfiles/kitty/kitty.conf" "$HOME/.config/kitty/"
+    # Update the path to the kitty icon in the kitty.desktop file
+    sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
+
+    ln -s "$HOME/dotfiles/kitty/kitty-color-schemes" "$HOME/.config/kitty/"
+    ln -s "$HOME/dotfiles/kitty/kitty.conf" "$HOME/.config/kitty/"
+fi
