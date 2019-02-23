d4dc937e122bf10c21efe4bc09b8a07e52645714
Darren-Haynes <darrenhaynes@zoho.com>
2019-02-23 11:47:53 -0800

    Create .local/bin and changed tildes to /home/darren.

--- install_scripts/install_dotfiles/kitty.sh
+++ install_scripts/install_dotfiles/kitty.sh
@@ -5,10 +5,11 @@ $CURL -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
 # your PATH)
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+mkdir -p "$HOME/.local/bin/"
+ln -s "$HOME/.local/kitty.app/bin/kitty" "$HOME/.local/bin/"
 
 # Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
+cp "$HOME/.local/kitty.app/share/applications/kitty.desktop" "$HOME/.local/share/applications"
 
 # Update the path to the kitty icon in the kitty.desktop file
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
