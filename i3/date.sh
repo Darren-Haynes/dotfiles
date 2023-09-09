23810e56ed6cab273c1def18a27537d1a1e201a3
Darren Haynes <darren@pop-os.localdomain>
2023-09-09 06:24:26 -0700

    Pruned Currently Unused Programs
    Getting back into linux and vim. Need to start off with a more basic config and readd as needed.

--- i3/date.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env/ bash
-#calender=""
-#getdate=$(date '+%a-%b-%d > %H:%M  ')
-getdate=$(date '+%a-%b-%d > %H:%M  ')
-date="$calender$getdate"
-color=$(xrdb -query | grep '*color3'| awk '{print $NF}')
-echo "<span foreground=\""$color"\">"$getdate"</span>"
--- i3/temperature.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env/ bash
-gettemp=$(acpi -t | awk '{print $4}')
-cent=$(acpi -t | awk '{print $6}')
-icon=""
-temp="$icon$gettemp$cent"
-color=$(xrdb -query | grep '*color3'| awk '{print $NF}')
-echo "<span foreground=\""$color"\">"$temp"</span>"
