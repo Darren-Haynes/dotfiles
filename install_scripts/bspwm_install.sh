e9590e7b81ab376a96e451b37a8a383c1ae7a8d8
Darren Haynes <darrenhaynes@zoho.com>
2023-10-10 20:01:04 -0700

    Initial commit of Bspwm main install script.

---
+++ install_scripts/bspwm_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    xinit \
+    lm-sensors \
+    mesa-utils \
+    x11-xserver-utils \
+    libxcb-xkb1 \
+    bspwm \
+    sxhkd \
+    curl \
+    wget \
+    feh \
+    imagemagick \
+    xz-utils \
+    xfce4-power-manager \
+    thunar \
+    gpick \
+    vim \
+    man
