f4cd464bf861174bead08d4f10793f583524f9e1
Darren Haynes <darrenhaynes@zoho.com>
2023-11-26 21:37:12 -0800

    Move apps to their own installation script.

---
+++ install_scripts/apt-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y \
+  imagemagick \         # cause it's awesome
+  thunar \              # xfce default file browser
+  gpick \               # gtk-based color picker
+  vim \                 # of course
+  mpv \                 # yep
+  bpytop \              # terminal-based system monitor
+  figlet \              # terminal-based ascii font generator
+  toilet                # works with figlet
+
