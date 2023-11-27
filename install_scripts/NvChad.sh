42f83233b596767013e87a901066926867d97519
Darren Haynes <darrenhaynes@zoho.com>
2023-11-26 21:35:12 -0800

    Install Script for  NvChad Neovim Config

---
+++ install_scripts/NvChad.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+  ripgrep
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
