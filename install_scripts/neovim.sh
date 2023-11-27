f2b10c449f941db10d09b0d1921385324ec16ceb
Darren Haynes <darrenhaynes@zoho.com>
2023-11-26 21:46:30 -0800

    Move install of xsel from main script to this script.

--- install_scripts/neovim.sh
+++ install_scripts/neovim.sh
@@ -6,6 +6,10 @@ os=$(grep -E ^ID= /etc/os-release)
 mkdir $HOME/tmp
 working_dir=$HOME/tmp
 if [[ "$os" = "ID=debian" ]]; then
+
+    sudo apt-get install -y \
+      xsel  # clipboard support
+
     echo "Installing Neovim for Debian"
     echo "$working_dir"
     url=https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage
@@ -14,8 +18,6 @@ if [[ "$os" = "ID=debian" ]]; then
     chmod u+x "$working_dir/nvim.appimage"
     sudo mv "$working_dir/nvim.appimage" /usr/local/bin
     rm -rf $working_dir
-    ln -s $HOME/Dotfiles/nvim $HOME/.config/
     echo "Neovim v0.9.4 has been install to /usr/local/bin"
 fi
-
 
