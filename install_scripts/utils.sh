e5cff1579063542004b6fa48e9b276b63e225d5d
Darren Haynes <darrenhaynes@zoho.com>
2023-11-26 21:41:37 -0800

    Move install of base utilites to it's own script.

---
+++ install_scripts/utils.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install -y \
+  xz-utils \        # compression utilities
+  unzip \           # unzip 
+  lm-sensors \      # hardware info
+  man \             # man pages
+  curl \            # download from web
+  wget              # aya download from web
+ 
