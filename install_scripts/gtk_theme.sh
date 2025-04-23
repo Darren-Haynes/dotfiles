1903fef415c8fcfc634727df31423ae1944e3c63
Darren Haynes <darren.haynes.mr@outlook.com>
2025-04-23 13:54:58 -0500

    Set gtk3 theme and icons.

---
+++ install_scripts/gtk_theme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.local/share/icons" ]; then 
+    mkdir $HOME/.local/share/icons
+fi
+
+if [ ! -d "$HOME/.local/share/themes" ]; then 
+    mkdir $HOME/.local/share/themes
+fi
+
+cp -R $HOME/Dotfiles/themes/Tokyonight-Dark-BL-LB $HOME/.local/share/themes/
+cp -R $HOME/Dotfiles/icons/Tokyonight-Dark $HOME/.local/share/icons/
+ln -s $HOME/Dotfiles/gtk-3.0/settings.ini $HOME/.config/gtk-3.0/settings.ini
