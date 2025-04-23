7f1d149a618b5f3af4510646673561179d3db76a
Darren Haynes <darren.haynes.mr@outlook.com>
2025-04-22 23:52:45 -0500

    Hopefully this fixes issue with pyenv executable not found. Thus 'wait' for the exec /bin/bash command to complete.

--- install_scripts/pyenv.sh
+++ install_scripts/pyenv.sh
@@ -55,10 +55,10 @@ else
 fi
 
 exec "$SHELL"
+wait
 pyenv install 3
 pyenv global 3
 pyenv install 3.12
-pyenv global 3
 mkdir -p $HOME/Code/Python/{Playground_3.12,Playground_3.13}
 pyenv virtualenv 3.12 playground_3.12
 pyenv virtualenv 3.13 playground_3.13
