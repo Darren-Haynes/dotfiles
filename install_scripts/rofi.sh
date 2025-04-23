4cbef2a98f0a154ad2e1ffa26abc78ee0d53a208
Darren Haynes <darren.haynes.mr@outlook.com>
2025-04-23 07:55:39 -0500

    Create config dir before installing rofi so that symlink works for config file.

--- install_scripts/rofi.sh
+++ install_scripts/rofi.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
+# Link config file before install. Otherwise install will be you to the create of the dir
+
+#!/bin/bash
+
+if [ ! -d "$HOME/.config/rofi" ]; then 
+    mkdir $HOME/.config/rofi
+    ln -s $HOME/Dotfiles/rofi/config.rasi $HOME/.config/rofi/
+fi
+
 sudo apt install -y rofi	       # application launcher and switcher
-ln -s $HOME/Dotfiles/rofi/config.rasi $HOME/.config/rofi/
+
+# Install tokyonight rofi theme
 mkdir $HOME/Downloads
 curl https://raw.githubusercontent.com/w8ste/Tokyonight-rofi-theme/refs/heads/main/tokyonight_big2.rasi > $HOME/Downloads/tokyonight.rasi
 sudo mv $HOME/Downloads/tokyonight.rasi /usr/share/rofi/themes
