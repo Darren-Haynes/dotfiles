7bca194f9eccf84e6f6aa3b1627b5ec0025af8dc
Darren Haynes <darren.haynes.mr@outlook.com>
2025-04-23 23:08:59 -0500

    Hopefully it works this time.

--- install_scripts/MS-Fonts.sh
+++ install_scripts/MS-Fonts.sh
@@ -2,7 +2,8 @@
 
 sudo apt-add-repository contrib non-free -y
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt-get install -y software-properties-common
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt-get install -y ttf-mscorefonts-installer
-
