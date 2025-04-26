bfd9f0bad9ee74b0fe3ecdc4216b60c9add53546
Darren Haynes <darren.haynes.mr@outlook.com>
2025-04-26 00:04:44 -0500

    Install gtk theme and icons.

--- install_scripts/gtk_theme.sh
+++ install_scripts/gtk_theme.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.local/share/icons" ]; then 
-    mkdir $HOME/.local/share/icons
+if [ ! -d "$HOME/.local/share/icons" ]; then
+  mkdir $HOME/.local/share/icons
 fi
 
-if [ ! -d "$HOME/.local/share/themes" ]; then 
-    mkdir $HOME/.local/share/themes
+if [ ! -d "$HOME/.local/share/themes" ]; then
+  mkdir $HOME/.local/share/themes
+fi
+
+if [ ! -d "$HOME/.config/gtk-3.0" ]; then
+  mkdir $HOME/.config/gtk-3.0
 fi
 
 cp -R $HOME/Dotfiles/themes/Tokyonight-Dark-BL-LB $HOME/.local/share/themes/
