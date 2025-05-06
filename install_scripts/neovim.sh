f92a6deb453fbac3acbd2276ce69974eae9a0cb6
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-06 07:05:26 -0500

    Add switch for install to x86 arch cpu's
    Previously I only installed on a VM within OSX using apple silicon as the cpu architexture. Now added an option to check if the host is running either 'arm' or 'x86'.

--- install_scripts/neovim.sh
+++ install_scripts/neovim.sh
@@ -19,28 +19,28 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
 if [ ! -d "$HOME/GitClones/Lazygit" ]; then
-  mkdir -p $HOME/GitClones/Lazygit
+  mkdir -p "$HOME/GitClones/Lazygit"
 fi
 
-cd $HOME/GitClones/Lazygit
-curl -Lo lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.49.0/lazygit_0.49.0_Linux_${arch}.tar.gz
+cd "$HOME/GitClones/Lazygit" || exit
+curl -Lo lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.49.0/lazygit_0.49.0_Linux_"${arch}".tar.gz
 tar xzf lazygit.tar.gz
 sudo install lazygit -D -t /usr/local/bin/
-cd $HOME
+cd "$HOME" || exit
 
 sudo apt-get install -y ssh-askpass-gnome
 sudo apt-get install -y ssh-askpass
 
 # Install and configure Neovim
-cd $HOME/Downloads
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-${arch}.tar.gz
+cd "$HOME/Downloads" || exit
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-"${arch}".tar.gz
 sudo rm -rf /opt/nvim*
-sudo tar -C /opt -xzf nvim-linux-${arch}.tar.gz
-rm nvim-linux-${arch}.tar.gz
-cd $HOME
+sudo tar -C /opt -xzf nvim-linux-"${arch}".tar.gz
+rm nvim-linux-"${arch}".tar.gz
+cd "$HOME" || exit
 
-printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>$HOME/.bashrc
-printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>$HOME/.profile
+printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>"$HOME/.bashrc"
+printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>"$HOME/.profile"
 
 #lazynvim
 mv ~/.config/nvim{,.bak}
@@ -50,8 +50,8 @@ mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
 
-git clone https://github.com/LazyVim/starter $HOME/.config/nvim
-rm -rf $HOME/.config/nvim/.git
+git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim/.git"
 
 # Pyenv virtual env default for neovim
 pyenv virtualenv 3.13.3 nvim-3.13.3.venv
@@ -59,8 +59,10 @@ pyenv activate nvim-3.13.3.venv
 pip install pynvim
 
 # Add personal configs to init.lua
-rm -f $HOME/.config/nvim/init.lua
-ln -s $HOME/Dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -s $HOME/Dotfiles/nvim/themes.lua $HOME/.config/nvim/lua/plugins/
+rm -f "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/Dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/Dotfiles/nvim/python.lua" "$HOME/.config/nvim/lua/plugins/"
+ln -s "$HOME/Dotfiles/nvim/themes.lua" "$HOME/.config/nvim/lua/plugins/"
+ln -s "$HOME/Dotfiles/nvim/zellij.lua" "$HOME/.config/nvim/lua/plugins/"
 
-source $HOME/.bashrc
+source "$HOME/.bashrc"
