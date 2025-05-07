b13cf2353e27a97850b922555de6992ffe94db2c
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-06 19:07:44 -0500

    ipython config.
    Initial commit of ipython profile config file and auto install of ipython config file.

--- install_scripts/config.sh
+++ install_scripts/config.sh
@@ -11,4 +11,13 @@ fi
 ln -s "$HOME/Dotfiles/aliases" "$HOME/.aliases"
 ln -s "$HOME/Dotfiles/X/xinitrc" "$HOME/.xinitrc"
 ln -s "$HOME/Dotfiles/X/Xresources" "$HOME/.Xresources"
+
+if [ ! -d "$HOME/.screenlayout" ]; then
+  mkdir "$HOME/.screenlayout"
+fi
 ln -s "$HOME/Dotfiles/screenlayout/LG-2k.sh" "$HOME/.screenlayout/LG-2k.sh"
+
+if [ ! -d "$HOME/.ipython/" ]; then
+  mkdir -p "$HOME/.ipython/"
+fi
+ln -s "$HOME/Dotfiles/ipython/profile_darren" "$HOME/.ipython/"
