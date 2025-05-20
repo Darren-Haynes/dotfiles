89db14b968b47ff1bcee24ba68be2fafd1782534
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-19 23:39:26 -0500

    File type for python and more.

--- install_scripts/neovim.sh
+++ install_scripts/neovim.sh
@@ -44,12 +44,24 @@ printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>"$HOME/.bashrc"
 printf '\nexport PATH="$PATH:/opt/nvim-linux-${arch}/bin"\n' >>"$HOME/.profile"
 
 #lazynvim
-mv ~/.config/nvim{,.bak}
+if [ -d "$HOME/.config/nvim.bak" ]; then
+  rm -rf "$HOME/.config/nvim.bak"
+  mv ~/.config/nvim{,.bak}
+fi
 
 # optional but recommended
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+if [ -d "$HOME/.local/share/nvim.bak" ]; then
+  rm -rf "$HOME/.local/share/nvim.bak"
+  mv ~/.local/share/nvim{,.bak}
+fi
+if [ -d "$HOME/.local/state/nvim.bak" ]; then
+  rm -rf "$HOME/.local/state/nvim.bak"
+  mv ~/.local/state/nvim{,.bak}
+fi
+if [ -d "$HOME/.cache/nvim.bak" ]; then
+  rm -rf "$HOME/.cache/nvim.bak"
+  mv ~/.cache/nvim{,.bak}
+fi
 
 git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
 rm -rf "$HOME/.config/nvim/.git"
@@ -62,10 +74,19 @@ pip install pynvim
 # Add personal configs to init.lua
 rm -f "$HOME/.config/nvim/init.lua"
 ln -s "$HOME/Dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+rm -f "$HOME/.config/nvim/lua/plugins/python.lua"
 ln -s "$HOME/Dotfiles/nvim/python.lua" "$HOME/.config/nvim/lua/plugins/"
+rm -f "$HOME/.config/nvim/lua/plugins/themes.lua"
 ln -s "$HOME/Dotfiles/nvim/themes.lua" "$HOME/.config/nvim/lua/plugins/"
+rm -f "$HOME/.config/nvim/lua/plugins/zellij.lua"
 ln -s "$HOME/Dotfiles/nvim/zellij.lua" "$HOME/.config/nvim/lua/plugins/"
 rm -f "$HOME/.config/nvim/lua/config/keymaps.lua"
 ln -s "$HOME/Dotfiles/nvim/keymaps.lua" "$HOME/.config/nvim/lua/config/"
+rm -f "$HOME/.config/nvim/lua/config/ui.lua"
+ln -s "$HOME/Dotfiles/nvim/ui.lua" "$HOME/.config/nvim/lua/plugins/"
 
+if [ ! -d "$HOME/.config/nvim/after/ftplugin" ]; then
+  mkdir -p "$HOME/.config/nvim/after/ftplugin/"
+  ln -s "$HOME/Dotfiles/nvim/ftplugin/python.lua" "$HOME/.config/nvim/lua/plugins/"
+fi
 source "$HOME/.bashrc"
