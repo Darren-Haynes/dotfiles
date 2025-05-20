c9e6e86f66193f24dc39e83a95094982a5eac9a4
Darren Haynes <darren.haynes.mr@outlook.com>
2025-05-19 23:41:44 -0500

    New themes, can't help myself.

--- nvim/themes.lua
+++ nvim/themes.lua
@@ -1,10 +1,26 @@
 return {
-	"folke/tokyonight.nvim",
-	opts = {
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
+	{ "samharju/synthweave.nvim" },
+	{ "ribru17/bamboo.nvim" },
+	{ "datsfilipe/vesper.nvim" },
+	{ "Everblush/nvim", name = "everblush" },
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		opts = {
+			transparent_background = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
 		},
 	},
 }
