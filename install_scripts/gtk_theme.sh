28e2d1de6c83caf9cfd890dc361078e8abacfd98
Darren Haynes <darren.haynes.mr@outlook.com>
2025-07-11 22:15:05 -0500

    Linter don't like no globbling.

--- install_scripts/gtk_theme.sh
+++ install_scripts/gtk_theme.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.local/share/icons" ]; then
-  mkdir -p $HOME/.local/share/icons
+  mkdir -p "$HOME/.local/share/icons"
 fi
 
 if [ ! -d "$HOME/.local/share/themes" ]; then
-  mkdir $HOME/.local/share/themes
+  mkdir "$HOME/.local/share/themes"
 fi
 
 if [ ! -d "$HOME/.config/gtk-3.0" ]; then
-  mkdir -p $HOME/.config/gtk-3.0
+  mkdir -p "$HOME/.config/gtk-3.0"
 fi
