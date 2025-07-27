51966abfd8438749087614793a9e593843a5ec5c
Darren Haynes <darren.haynes.mr@outlook.com>
2025-07-26 19:12:59 -0500

    Dunst notifications.

--- bin/network-restart.sh
+++ bin/network-restart.sh
@@ -5,19 +5,14 @@ set -eu
 icon1="$HOME/Dotfiles/icons/network-1.png"
 icon2="$HOME/Dotfiles/icons/network-true.png"
 
-status="$(systemctl status networking | grep Active | awk '{print $2}')"
-if [ "$status" == "active" ]; then
-  dunstify --timeout=9000 --icon="$icon1" "Network already active"
-fi
-
 dunstify --timeout=9000 --icon="$icon1" "Network restart intiated"
 
 restart_not_complete=true
 while [ "$restart_not_complete" ]; do
-  sudo systemctl restart networking
+  sudo systemctl restart NetworkManager
   dunstify --timeout=9000 --icon="$icon2" "Network restart completed"
   wait 1
-  status="$(systemctl status networking | grep Active | awk '{print $2}')"
+  status="$(systemctl status NetworkManager | grep Active | awk '{print $2}')"
   dunstify --timeout=9000 --icon="$icon2" "Network restart completed"
   if [ "$status" == "active" ]; then
     dunstify --timeout=9000 --icon="$icon2" "Network restart completed"
