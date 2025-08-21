811aa7cbf5bc6e4c187bd2cecf365e0ac8045cb4
Darren Haynes <darren.haynes.mr@outlook.com>
2025-08-21 16:05:45 -0500

    Prep for Debian 13.

---
+++ install_scripts/apt-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y flameshot        # dem screenshots
+sudo apt install -y gpick            # gtk-based color picker
+sudo apt install -y imagemagick      # cause it's awesome
+sudo apt install -y mpv              # yep
+sudo apt install -y ncal             # what day is it?
+sudo apt install -y pavucontrol-qt   # cute sound control
+sudo apt install -y speedcrunch      # a proper tidy calculator
+sudo apt install -y sqlite3          # He ain't heavy, he's my brother
+sudo apt install -y transmission-cli # Torrents and magnets
+sudo apt install -y vim              # of course
