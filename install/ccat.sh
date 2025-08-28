553703c1374b2ed2367230af4e7861453a3c4100
Darren Haynes <darren.haynes.mr@outlook.com>
2025-08-28 17:38:11 -0500

    Changes for Deb 13 Trixie.

--- install/ccat.sh
+++ install/ccat.sh
@@ -5,3 +5,7 @@ curl -L https://github.com/owenthereal/ccat/releases/download/v1.1.0/linux-amd64
 chmod a+rx ccat
 sudo mv ccat /opt/
 cd "$HOME"
+
+# opt path
+export PATH="$PATH:/opt"
+printf '\nexport PATH="$PATH/opt"\n' >>"$HOME/.bashrc"
