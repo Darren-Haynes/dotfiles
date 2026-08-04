f81379fdb717480d6b1bf504728c07cea1b60043
Darren Haynes <darren.haynes.mr@outlook.com>
2026-08-04 17:01:01 -0500

    feat: debug keybinds override and diable tabline by default.

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -5,16 +5,33 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- 1. Enable Native Format-on-Save
+    -- STRUCTURAL GLOBAL OPTIONS (HIDE TABLINE BY DEFAULT)
+    options = {
+      opt = {
+        showtabline = 0, -- 0 completely hides the tabline on boot
+      },
+    },
+
+    -- ENABLE NATIVE FORMAT-ON-SAVE
     formatting = {
       format_on_save = {
-        enabled = true, -- Automatically trigger formatting when saving files
+        enabled = true,
         allow_filetypes = {}, -- Empty table means it runs on ALL supported filetypes (like TS/JS)
       },
       timeout_ms = 3000, -- Give formatters like Prettier/Stylua 3 seconds before timing out
     },
 
-    -- 2. Clean up trailing whitespace using Neovim Autocommands
+    -- CUSTOM DEBUGGER KEYMAP MAPPINGS
+    mappings = {
+      -- Normal mode keymaps
+      n = {
+        ["<Leader>dj"] = { function() require("dap").step_over() end, desc = "Step over" },
+        ["<Leader>do"] = { function() require("dap").step_out() end, desc = "Step out" },
+        ["<Leader>dO"] = false,
+      },
+    },
+
+    -- CLEAN UP TRAILING WHITESPACE USING NEOVIM AUTOCOMMANDS
     autocmds = {
       -- Create a custom group so these functions don't duplicate on reload
       whitespace_cleanup = {
