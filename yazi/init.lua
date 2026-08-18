a6a7585530662b002d2622128da8fefbf3059ab1
Darren Haynes <darren.haynes.mr@outlook.com>
2026-08-18 15:04:29 -0500

    (feat): yazi plugins added and ayu theme

--- yazi/init.lua
+++ yazi/init.lua
@@ -39,3 +39,18 @@ Status:children_add(function(self)
 		return ""
 	end
 end, 3300, Status.LEFT)
+
+-- Calculate size mod time of file
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
